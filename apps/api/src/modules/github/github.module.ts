5e4a120c86f2c7bac468e8a46f5a77ae0fd7dea4
nexoraprojecto6-boop <nexoraprojecto6@gmail.com>
2026-09-23 22:13:56 +0100

    Create github.module.ts

---
+++ apps/api/src/modules/github/github.module.ts
@@ -0,0 +1,10 @@
+import { Module } from "@nestjs/common";
+import { GitHubController } from "./github.controller";
+import { GitHubService } from "./github.service";
+
+@Module({
+  controllers: [GitHubController],
+  providers: [GitHubService],
+  exports: [GitHubService],
+})
+export class GitHubModule {}
