b8693071fa3d32cd580a2f39ebf4a5e4b61aaa9c
nexoraprojecto6-boop <nexoraprojecto6@gmail.com>
2026-09-23 23:04:35 +0100

    Update app.module.ts

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from "@nestjs/common";
 import { PrismaModule } from "@kixihost/database";
 import { AuthModule } from "./modules/auth/auth.module";
 import { GitHubModule } from "./modules/github/github.module";
+import { ProjectsModule } from "./modules/projects/projects.module";
 
 @Module({
-  imports: [PrismaModule, AuthModule, GitHubModule],
+  imports: [PrismaModule, AuthModule, GitHubModule, ProjectsModule],
 })
-export class AppModule {}
+export class ProjectsModuleRoot {}
