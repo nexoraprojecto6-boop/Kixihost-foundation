3ccb114c10570388947aba013400d9926a37a6f2
nexoraprojecto6-boop <nexoraprojecto6@gmail.com>
2026-09-23 22:19:17 +0100

    Update main.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,13 +6,24 @@
 // dos clientes acontece no Data Plane, fora deste processo.
 
 import "reflect-metadata";
-// TODO(Fase 2): import { NestFactory } from "@nestjs/core";
-// TODO(Fase 2): import { AppModule } from "./app.module";
-//
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//   await app.listen(process.env.PORT ?? 4000);
-// }
-// bootstrap();
+import { NestFactory } from "@nestjs/core";
+import cookieParser from "cookie-parser";
+import { AppModule } from "./app.module";
 
-export {};
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { rawBody: true });
+
+  app.use(cookieParser());
+
+  app.enableCors({
+    origin: [process.env.APP_URL ?? "http://localhost:3000"],
+    credentials: true,
+  });
+
+  const port = process.env.PORT ?? 4000;
+  await app.listen(port);
+  // eslint-disable-next-line no-console
+  console.log(`KixiHost API a correr na porta ${port}`);
+}
+
+bootstrap();
