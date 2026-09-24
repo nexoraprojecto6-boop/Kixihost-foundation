c1045493f82ac163fb832123df41c3cf52e887cf
nexoraprojecto6-boop <nexoraprojecto6@gmail.com>
2026-09-24 23:47:43 +0100

    Update app.module.ts

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,8 +3,9 @@ import { PrismaModule } from "@kixihost/database";
 import { AuthModule } from "./modules/auth/auth.module";
 import { GitHubModule } from "./modules/github/github.module";
 import { ProjectsModule } from "./modules/projects/projects.module";
+import { DeploymentsModule } from "./modules/deployments/deployments.module";
 
 @Module({
-  imports: [PrismaModule, AuthModule, GitHubModule, ProjectsModule],
+  imports: [PrismaModule, AuthModule, GitHubModule, ProjectsModule, DeploymentsModule],
 })
 export class AppModule {}
