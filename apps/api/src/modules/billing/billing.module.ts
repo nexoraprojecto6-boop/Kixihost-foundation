0db9054646639aa3d971963161995703cb831092
nexoraprojecto6-boop <nexoraprojecto6@gmail.com>
2026-09-25 23:01:38 +0100

    Update billing.module.ts

--- apps/api/src/modules/billing/billing.module.ts
+++ apps/api/src/modules/billing/billing.module.ts
@@ -2,10 +2,11 @@ import { Module } from "@nestjs/common";
 import { AuthModule } from "../auth/auth.module";
 import { BillingController } from "./billing.controller";
 import { BillingService } from "./billing.service";
+import { PayPayController } from "./paypay.controller";
 
 @Module({
   imports: [AuthModule],
-  controllers: [BillingController],
+  controllers: [BillingController, PayPayController],
   providers: [BillingService],
   exports: [BillingService],
 })
