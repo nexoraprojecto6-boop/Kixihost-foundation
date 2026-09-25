298961c7624178394ec1acb406903862135c78e2
nexoraprojecto6-boop <nexoraprojecto6@gmail.com>
2026-09-25 23:23:18 +0100

    Update auth.module.ts

--- apps/api/src/modules/auth/auth.module.ts
+++ apps/api/src/modules/auth/auth.module.ts
@@ -1,12 +1,7 @@
 import { Module } from "@nestjs/common";
-import { AuthController } from "./auth.controller";
-import { AuthService } from "./auth.service";
-import { SessionAuthService } from "./session-auth.service";
-import { SessionGuard } from "./session.guard";
+import { HealthController } from "./health.controller";
 
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService, SessionAuthService, SessionGuard],
-  exports: [AuthService, SessionAuthService, SessionGuard],
+  controllers: [HealthController],
 })
-export class AuthModule {}
+export class HealthModule {}
